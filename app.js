51a3369c19fe6d081a45e167531fadbf2459c912
Ali Kahwaji <ali.kahwaji@hotmail.com>
2017-10-10 18:25:23 +0400

    Priniting the lat and lng with location

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=72%20neslon%20street%20auckland%20new%20zealand',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(response, undefined, 2))
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+    console.log(`longitude: ${body.results[0].geometry.location.lng}`)
 })
